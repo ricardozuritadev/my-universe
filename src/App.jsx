b97e126aa6389d84a7541b6c418f6bf5edf4064f
rizuba <r.zurita@cleverpy.com>
2023-03-23 17:21:39 +0100

    Set camera defaults

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Universe from './components/universe/universe';
 
 const App = () => {
   return (
-    <Canvas>
+    <Canvas camera={{ position: [-1.5, 1, 15], fov: 45, near: 1, far: 50 }}>
       <Universe />
     </Canvas>
   );
