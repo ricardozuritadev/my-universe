b9c60c251131aa81643ef52efce0c97d80ec41e9
rizuba <r.zurita@cleverpy.com>
2023-03-23 17:22:24 +0100

    Implement test objects

--- src/components/universe/universe.jsx
+++ src/components/universe/universe.jsx
@@ -1,10 +1,33 @@
+import CameraRig from '../camera-rig/camera-rig';
+
 const Universe = () => {
   return (
     <>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[-2, 0, 3]} intensity={1.5} />
+
       <mesh>
-        <sphereGeometry />
-        <meshBasicMaterial color="white" />
+        <boxGeometry />
+        <meshStandardMaterial />
       </mesh>
+
+      <mesh position={[6, 3, 0]}>
+        <sphereGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+
+      <mesh position={[-5, 3, 0]}>
+        <sphereGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+
+      <mesh position={[-6, -3, 0]}>
+        <sphereGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+
+      {/* Camera movements */}
+      <CameraRig />
     </>
   );
 };
