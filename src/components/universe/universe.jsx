5b3f412407ae4f7f5916d93ba89a97fd8355475e
rizuba <r.zurita@cleverpy.com>
2023-03-30 15:27:58 +0200

    feature: implement solar system component

--- src/components/universe/universe.jsx
+++ src/components/universe/universe.jsx
@@ -1,42 +1,29 @@
+import { OrbitControls, Stars, Html } from '@react-three/drei';
+
 import CameraRig from '../camera-rig/camera-rig';
-import { OrbitControls } from '@react-three/drei';
+import SolarSystem from '../solar-system/solar-system.component';
 
 const Universe = () => {
   return (
     <>
+      {/* <OrbitControls /> */}
+      {/* Camera movements */}
+      <CameraRig />
+
       <ambientLight intensity={0.5} />
       <directionalLight position={[-2, 0, 3]} intensity={1.5} />
 
-      <group position-y={1} rotation={[0.4, 0, 0.2]}>
-        <mesh>
-          <boxGeometry />
-          <meshStandardMaterial />
-        </mesh>
+      <Stars
+        radius={50}
+        depth={20}
+        count={5000}
+        factor={4}
+        saturation={0}
+        fade
+        speed={0}
+      />
 
-        <mesh position={[0, 0, 6]}>
-          <sphereGeometry />
-          <meshStandardMaterial color="yellowgreen" />
-        </mesh>
-
-        <mesh position={[9, 0, 0]}>
-          <sphereGeometry />
-          <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-
-        <mesh position={[-8, 0, 0]}>
-          <sphereGeometry />
-          <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-
-        <mesh position={[0, 0, -7]}>
-          <sphereGeometry />
-          <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-      </group>
-
-      {/* Camera movements */}
-      <CameraRig />
-      {/* <OrbitControls /> */}
+      <SolarSystem />
     </>
   );
 };
