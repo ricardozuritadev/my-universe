0f880567ca442d602b4a5aeb40faef2feb3bdeb3
rizuba <r.zurita@cleverpy.com>
2023-03-30 18:47:09 +0200

    feature: implement welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,23 @@ import { Canvas } from '@react-three/fiber';
 import { PerspectiveCamera } from '@react-three/drei';
 
 import Universe from './components/universe/universe';
+import WelcomePage from './pages/welcome-page/welcome-page';
 
 const App = () => {
   return (
-    <Canvas>
-      <PerspectiveCamera
-        makeDefault
-        position={[1.5, 1.5, 15]}
-        fov={45}
-        near={1}
-        far={160}
-      />
-      <Universe />
-    </Canvas>
+    <>
+      <WelcomePage />
+      <Canvas>
+        <PerspectiveCamera
+          makeDefault
+          position={[1.5, 1.5, 15]}
+          fov={45}
+          near={1}
+          far={160}
+        />
+        <Universe />
+      </Canvas>
+    </>
   );
 };
 
