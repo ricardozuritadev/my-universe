0645b050bc749e5b287e6f3a66205690785affbb
rizuba <r.zurita@cleverpy.com>
2023-03-30 13:22:17 +0200

    feature: implement perspective camera

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import { Canvas } from '@react-three/fiber';
+import { PerspectiveCamera } from '@react-three/drei';
+
 import Universe from './components/universe/universe';
 
 const App = () => {
   return (
-    <Canvas camera={{ position: [-1.5, 1, 15], fov: 45, near: 1, far: 50 }}>
+    <Canvas>
+      <PerspectiveCamera
+        makeDefault
+        position={[1.5, 1.5, 15]}
+        fov={45}
+        near={1}
+        far={50}
+      />
       <Universe />
     </Canvas>
   );
