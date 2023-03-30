a066ba3e2881a4e52f2c2cabe4faa3bcfcfa5b63
rizuba <r.zurita@cleverpy.com>
2023-03-30 18:48:05 +0200

    feature: create welcome page component

--- src/pages/welcome-page/welcome-page.jsx
+++ src/pages/welcome-page/welcome-page.jsx
@@ -1,5 +1,38 @@
+import { useState } from 'react';
+
 const WelcomePage = () => {
-  return <div>WelcomePage</div>;
+  const [welcomePageVisibility, setWelcomePageVisibility] = useState({
+    opacity: 1,
+    display: 'block',
+  });
+
+  return (
+    <div
+      className="p-welcome-page"
+      style={{
+        opacity: welcomePageVisibility.opacity,
+        display: welcomePageVisibility.display,
+      }}
+    >
+      <p className="p-welcome-page__text p-welcome-page__text--welcome">
+        bienvenido/a a
+      </p>
+      <h2 className="p-welcome-page__text p-welcome-page__text--universe">
+        mi universo
+      </h2>
+      <button
+        className="p-welcome-page__button"
+        onClick={() => {
+          setWelcomePageVisibility({
+            opacity: 0,
+          });
+          setTimeout(() => setWelcomePageVisibility({ display: 'none' }), 1000);
+        }}
+      >
+        explorar
+      </button>
+    </div>
+  );
 };
 
 export default WelcomePage;
