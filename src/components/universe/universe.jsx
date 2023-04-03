84c7b4ebbea1f17d36e3afab26a54586cdab4bb7
rizuba <r.zurita@cleverpy.com>
2023-04-03 09:32:28 +0200

    feature: implement debug panel cameras

--- src/components/universe/universe.jsx
+++ src/components/universe/universe.jsx
@@ -1,14 +1,40 @@
+import { useEffect, useState } from 'react';
+import { useControls } from 'leva';
+
 import { OrbitControls, Stars } from '@react-three/drei';
+
+import {
+  PRODUCTION_CAMERA,
+  DEBUG_CAMERA,
+} from '../../constants/universe.constants';
 
 import CameraRig from '../camera-rig/camera-rig';
 import SolarSystem from '../solar-system/solar-system.component';
 
 const Universe = () => {
+  const [isProductionCamera, setIsProductionCamera] = useState(false);
+  const [isDebugCamera, setIsDebugCamera] = useState(true);
+
+  const { cameraType } = useControls('Cameras', {
+    cameraType: {
+      options: [PRODUCTION_CAMERA, DEBUG_CAMERA],
+    },
+  });
+
+  useEffect(() => {
+    if (cameraType === PRODUCTION_CAMERA) {
+      setIsProductionCamera(true);
+      setIsDebugCamera(false);
+    } else {
+      setIsProductionCamera(false);
+      setIsDebugCamera(true);
+    }
+  }, [cameraType]);
+
   return (
     <>
-      {/* <OrbitControls /> */}
-      {/* Camera movements */}
-      <CameraRig />
+      {isDebugCamera && <OrbitControls />}
+      {isProductionCamera && <CameraRig />}
 
       <ambientLight intensity={0.5} />
       <directionalLight position={[1, 5, 10]} intensity={1} />
