ceaac95d8dac091132fc0e9d4c362134437f1ee0
rizuba <r.zurita@cleverpy.com>
2023-04-03 09:28:53 +0200

    feature: implement debug and performance panels

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
+import { useControls } from 'leva';
+
 import { Canvas } from '@react-three/fiber';
+import { Leva } from 'leva';
+import { Perf } from 'r3f-perf';
 import { PerspectiveCamera } from '@react-three/drei';
 
 import Universe from './components/universe/universe';
 import WelcomePage from './pages/welcome-page/welcome-page';
 
 const App = () => {
+  const { perfPanel } = useControls({
+    perfPanel: {
+      name: 'Show performance panel',
+      value: true,
+    },
+  });
+
+  console.log('=> perfPanel: ', perfPanel);
+
   return (
     <>
+      <Leva collapsed />
       <WelcomePage />
       <Canvas>
+        {perfPanel && <Perf position="top-left" />}
         <PerspectiveCamera
           makeDefault
           position={[1.5, 1.5, 15]}
