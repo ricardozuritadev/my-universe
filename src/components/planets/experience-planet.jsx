e3c21473092bff7468c709133bf58fb83301673a
rizuba <r.zurita@cleverpy.com>
2023-04-03 09:29:59 +0200

    feature: create experience planet component

---
+++ src/components/planets/experience-planet.jsx
@@ -0,0 +1,10 @@
+const ExperiencePLanet = () => {
+  return (
+    <mesh position={[8, 0, 0]}>
+      <sphereGeometry />
+      <meshStandardMaterial color="mediumpurple" />
+    </mesh>
+  );
+};
+
+export default ExperiencePLanet;
