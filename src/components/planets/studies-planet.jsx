6f047db15ed9366392a32c01b522bdbc615dbf20
rizuba <r.zurita@cleverpy.com>
2023-04-03 12:24:05 +0200

    feature: implement debug panel

--- src/components/planets/studies-planet.jsx
+++ src/components/planets/studies-planet.jsx
@@ -1,6 +1,18 @@
+import { useControls } from 'leva';
+
 const StudiesPlanet = () => {
+  const { position } = useControls('Studies planet', {
+    position: {
+      value: {
+        x: -6.5,
+        y: 0,
+        z: 0,
+      },
+    },
+  });
+
   return (
-    <mesh position={[-7, 0, 0]}>
+    <mesh position={[position.x, position.y, position.z]}>
       <sphereGeometry />
       <meshStandardMaterial color="mediumpurple" />
     </mesh>
