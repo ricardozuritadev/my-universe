05bd9049691046206ea047318d3dc29fa0bd97a3
rizuba <r.zurita@cleverpy.com>
2023-04-03 12:24:25 +0200

    feature: implement debug panel

--- src/components/solar-system/solar-system.component.jsx
+++ src/components/solar-system/solar-system.component.jsx
@@ -1,3 +1,5 @@
+import { useControls } from 'leva';
+
 import Sun from '../sun/sun';
 import SkillsPlanet from '../planets/skills-planet';
 import ExperiencePLanet from '../planets/experience-planet';
@@ -5,8 +7,30 @@ import StudiesPlanet from '../planets/studies-planet';
 import AboutPlanet from '../planets/about-planet';
 
 const SolarSystem = () => {
+  const { position, rotation } = useControls('Solar system', {
+    position: {
+      name: 'Position Y',
+      value: {
+        x: 0,
+        y: 1.2,
+        z: 0,
+      },
+    },
+    rotation: {
+      name: 'Rotation',
+      value: {
+        x: 0.45,
+        y: 0.1,
+        z: 0.15,
+      },
+    },
+  });
+
   return (
-    <group position-y={1} rotation={[0.4, 0, 0.2]}>
+    <group
+      position={[position.x, position.y, position.z]}
+      rotation={[rotation.x, rotation.y, rotation.z]}
+    >
       <Sun />
       <SkillsPlanet />
       <ExperiencePLanet />
